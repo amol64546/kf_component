ad1ecb1bdffd44871a615165c2fd745a1aad372b
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-10-11 16:00:19 +0530

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -43,6 +43,11 @@ if [ $? -eq 0 ]; then
   curl --location --globoff --request POST "https://ig.aidtaas.com/bob-service/v1.0/ml/brick/image/$IMAGE_STATUS_ID?status=COMPLETED" \
   --data ''
   echo "Status update: COMPLETED"
+
+  # Remove the local Docker image
+  echo "Removing local Docker image..."
+  sudo docker rmi "$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+  
 else
   echo "Docker image build and push failed."
 
